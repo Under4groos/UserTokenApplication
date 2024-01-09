34cc28fcb7d380f12eaa527768bb5fd4a2cc7996
UnderKo <hhit934@gmail.com>
2024-01-09 23:28:49 +0300

    dsa

--- UserToken/Modules/ModulUser.cs
+++ UserToken/Modules/ModulUser.cs
@@ -1,7 +1,22 @@
 ﻿namespace UserToken.Modules
 {
+    public enum UserAcces
+    {
+        user,
+        admin 
+    }
+
     public class ModulUser
     {
+
+        /// <summary>
+        /// 1. User name 
+        /// 2. User pasword 
+        /// </summary>
+        Dictionary<string, (string, string , UserAcces)> Users = new Dictionary<string, (string, string , UserAcces)>();
+
+
+
 
         private string Reverse(string s)
         {
@@ -9,36 +24,91 @@
             Array.Reverse(charArray);
             return new string(charArray);
         }
+        string _create_token(string username, string userpassword)
+        {
+            return Reverse(username + userpassword);
+        }
+        
+        void _registr_user(string username, string userpassword , UserAcces userAcces)
+        {
 
-        /// <summary>
-        /// 1. User name 
-        /// 2. User pasword 
-        /// </summary>
-        Dictionary<string, (string , string)> Users = new Dictionary<string, (string , string)>();
+            if (Users.ContainsKey(username))
+            {
+                Users[username] = (userpassword, _create_token(username, userpassword), userAcces);
+            }
+            else
+            {
+                Users.Add(username, (userpassword, _create_token(username, userpassword), userAcces));
+            }
+
+            
+
+
+
+
+        }
+
+
+        Random random = new Random();
+
         public ModulUser(WebApplication webApplication , string prefix = "/")
         {
-            var todosApi = webApplication.MapGroup(prefix);
-            todosApi.MapGet("/registr", (string username , string userpassword) =>
+            string _daata_password = "";
+            for (int i = 0; i < 10; i++)
             {
-                string data_ = username + userpassword;
+                _daata_password = "";
+                for (int d = 0; d < 10; d++)
+                {
+                    _daata_password += random.Next(0, 100);
+                }
+                _registr_user($"username-{i}", _daata_password , UserAcces.user);
+            }
+           
 
-                string newdata_ = Reverse(data_);
 
 
-                Users.Add(username, (userpassword, newdata_));
+            var todosApi = webApplication.MapGroup(prefix);
+            todosApi.MapGet("/registr", (string username , string userpassword , int UserAcces) =>
+            {
 
-                return $"{data_} : {newdata_}";
+                _registr_user(username, userpassword , (UserAcces)UserAcces);
+
+
+
 
             });
+            todosApi.MapGet("/Login", (string username, string userpassword) =>
+            {
+                if (Users.ContainsKey(username))
+                {
+                    if (Users[username].Item1 == userpassword)
+                    {
+                        return Users[username].Item2;
+                    }
+                }
+                return "Error! Вы ввели не тот пароль ибо ... ";
+
+            });
+
+
+            /// Хочу получить информацию о человеке имеея токен админа и имя пользователя
+            todosApi.MapGet("/GetUser/", (string token , string username) =>
+            {
+                foreach (var item in Users)
+                {
+                    
+                }
+            });
+
+
+
+
             todosApi.MapGet("/GetUsers", () =>
             {
                 string data_ = "";
                 foreach (var item in Users)
                 {
-
-
-
-                    data_ += $"{item.Key} ";
+                    data_ += $"UserName: {item.Key}\n\tUserPass: {item.Value.Item1}\n\tUser Token: {item.Value.Item2}\n\tUser atc: {item.Value.Item3}\n";
                 }
                 return data_;
             });
